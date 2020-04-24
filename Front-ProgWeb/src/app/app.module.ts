78c8d72753a4773e6a2e9020e1f900d69191ec8f
Manuel Sanchez <pipesan9710@gmail.com>
2020-04-24 17:22:52 -0500

    First commit

---
+++ Front-ProgWeb/src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app.routing';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+
+import { ComponentsModule } from './components/components.module';
+import { ExamplesModule } from './examples/examples.module';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { RegisterComponent } from './components/register/register.component';
+import { SignupComponent } from './components/signup/signup.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    ContactUsComponent,
+    RegisterComponent,
+    SignupComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgbModule,
+    FormsModule,
+    RouterModule,
+    ComponentsModule,
+    ExamplesModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Front-ProgWeb/src/app/app.routing.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { CommonModule, } from '@angular/common';
+import { BrowserModule  } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+
+import { ComponentsComponent } from './components/components.component';
+import { ProfileComponent } from './examples/profile/profile.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { LandingComponent } from './examples/landing/landing.component';
+import { NucleoiconsComponent } from './components/nucleoicons/nucleoicons.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { RegisterComponent } from './components/register/register.component';
+
+const routes: Routes =[
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home',             component: ComponentsComponent },
+    { path: 'user-profile',     component: ProfileComponent },
+    { path: 'signup',           component: SignupComponent },
+    { path: 'register',           component: RegisterComponent },
+    { path: 'landing',          component: LandingComponent },
+    { path: 'nucleoicons',      component: NucleoiconsComponent },
+    { path: 'contact-us',      component: ContactUsComponent }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BrowserModule,
+    RouterModule.forRoot(routes,{
+      useHash: true
+    })
+  ],
+  exports: [
+  ],
+})
+export class AppRoutingModule { }
